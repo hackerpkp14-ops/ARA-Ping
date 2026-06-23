3bb98b5235bfbdda6f94bec94353f03cd897b81e
Shivam <hackerpkp14@gmail.com>
2026-06-23 21:29:16 +0530

    small error

--- server.js
+++ server.js
@@ -100,27 +100,7 @@ const Message = mongoose.model("Message", MessageSchema);
    MULTER IMAGE STORAGE
 ====================== */
 
-const cloudinary =
-require("cloudinary").v2;
 
-const {
-  CloudinaryStorage
-} = require(
-  "multer-storage-cloudinary"
-);
-
-cloudinary.config({
-
-  cloud_name:
-  process.env.CLOUDINARY_CLOUD_NAME,
-
-  api_key:
-  process.env.CLOUDINARY_API_KEY,
-
-  api_secret:
-  process.env.CLOUDINARY_API_SECRET
-
-});
 
 const storage =
 new CloudinaryStorage({
