33d7aa00c4412dcfa441c75237b235e5dbf82d2e
Shivam <hackerpkp14@gmail.com>
2026-06-23 21:45:05 +0530

    Fix Cloudinary integration

--- server.js
+++ server.js
@@ -9,26 +9,7 @@ const fs = require("fs");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const cloudinary =
-require("cloudinary").v2;
 
-const {
-CloudinaryStorage
-} = require(
-"multer-storage-cloudinary"
-);
-cloudinary.config({
-
-  cloud_name:
-  process.env.CLOUDINARY_CLOUD_NAME,
-
-  api_key:
-  process.env.CLOUDINARY_API_KEY,
-
-  api_secret:
-  process.env.CLOUDINARY_API_SECRET
-
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -100,7 +81,26 @@ const Message = mongoose.model("Message", MessageSchema);
    MULTER IMAGE STORAGE
 ====================== */
 
+const cloudinary =
+require("cloudinary").v2;
 
+const CloudinaryStorage =
+require(
+  "multer-storage-cloudinary"
+);
+
+cloudinary.config({
+
+  cloud_name:
+  process.env.CLOUDINARY_CLOUD_NAME,
+
+  api_key:
+  process.env.CLOUDINARY_API_KEY,
+
+  api_secret:
+  process.env.CLOUDINARY_API_SECRET
+
+});
 
 const storage =
 new CloudinaryStorage({
@@ -108,15 +108,13 @@ new CloudinaryStorage({
   cloudinary,
 
   params: {
-
-    folder:
-    "ara-ping"
-
+    folder: "ara-ping"
   }
 
 });
 
-const upload = multer({
+const upload =
+multer({
   storage
 });
 
