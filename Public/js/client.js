06574a1e73b0adbde817871e5c8551be6fb6c7c9
Shivam <hackerpkp14@gmail.com>
2026-07-12 21:16:45 +0530

    restructuring 4

--- Public/js/client.js
+++ Public/js/client.js
@@ -213,89 +213,6 @@ async function loadRecentChats() {
 }
 // SIDEBAR USER
 
-function addUserToSidebar(user) {
-
-  const already =
-  sidebarUsers[user.username];
-
-if (already) {
-
-  updateSidebarUser(user);
-
-  return;
-
-}
-
-  const div =
-    document.createElement("div");
-
-  div.className = "user";
-
-  div.dataset.username =
-    user.username;
-
-  const time =
-    user.lastTime
-    ? new Date(user.lastTime)
-        .toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit"
-        })
-    : "";
-
-  div.innerHTML = `
-
-    <img
-      class="sidebarProfile"
-      src="${
-        user.profilePic ||
-        "https://ui-avatars.com/api/?name=" +
-        encodeURIComponent(user.username)
-      }"
-    >
-
-    <div class="userInfo">
-
-      <div class="userTop">
-
-        <div class="userName">
-          ${user.username}
-        </div>
-
-        <div class="chatTime">
-          ${time}
-        </div>
-
-      </div>
-
-      <div class="userBottom">
-
-        <div class="lastMsg">
-          ${user.lastMessage || ""}
-        </div>
-
-        ${
-          user.unread > 0
-          ? `<div class="unreadBadge">${user.unread}</div>`
-          : ""
-        }
-
-      </div>
-
-    </div>
-
-  `;
-
-  div.onclick = () => {
-
-  openChat(user.username);
-
-};
-sidebarUsers[user.username] = div;
-
-usersBox.prepend(div);
-}
-
 function updateSidebarUser(user) {
 
   const div = sidebarUsers[user.username];
@@ -310,27 +227,12 @@ function updateSidebarUser(user) {
   if (user.lastTime) {
 
     div.querySelector(".chatTime").innerText =
-      new Date(user.lastTime)
-        .toLocaleTimeString([], {
-
-          hour: "2-digit",
-
-          minute: "2-digit"
-
-        });
+      new Date(user.lastTime).toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+      });
 
   }
-  function moveUserToTop(username){
-
-  const div = sidebarUsers[username];
-
-  if(!div) return;
-
-  usersBox.prepend(div);
-
-  }
-}
-
 
   // Update unread badge
   let badge =
@@ -340,11 +242,9 @@ function updateSidebarUser(user) {
 
     if (!badge) {
 
-      badge =
-        document.createElement("div");
+      badge = document.createElement("div");
 
-      badge.className =
-        "unreadBadge";
+      badge.className = "unreadBadge";
 
       div.querySelector(".userBottom")
         .appendChild(badge);
@@ -359,8 +259,20 @@ function updateSidebarUser(user) {
 
   }
 
-  // Move chat to top
+  moveUserToTop(user.username);
+
+}
+
+function moveUserToTop(username) {
+
+  const div = sidebarUsers[username];
+
+  if (!div) return;
+
   usersBox.prepend(div);
+
+}
+
 
 
 
