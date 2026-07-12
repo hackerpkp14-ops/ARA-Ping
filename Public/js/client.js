23c5e557c7b2fb85bf4c4ada5aaa55a35802d27a
Shivam <hackerpkp14@gmail.com>
2026-07-12 21:24:12 +0530

    restructuring 5

--- Public/js/client.js
+++ Public/js/client.js
@@ -212,6 +212,85 @@ async function loadRecentChats() {
 
 }
 // SIDEBAR USER
+function addUserToSidebar(user) {
+
+  const already = sidebarUsers[user.username];
+
+  if (already) {
+
+    updateSidebarUser(user);
+
+    return;
+
+  }
+
+  const div = document.createElement("div");
+
+  div.className = "user";
+
+  div.dataset.username = user.username;
+
+  const time = user.lastTime
+    ? new Date(user.lastTime).toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+      })
+    : "";
+
+  div.innerHTML = `
+
+    <img
+      class="sidebarProfile"
+      src="${
+        user.profilePic ||
+        "https://ui-avatars.com/api/?name=" +
+        encodeURIComponent(user.username)
+      }"
+    >
+
+    <div class="userInfo">
+
+      <div class="userTop">
+
+        <div class="userName">
+          ${user.username}
+        </div>
+
+        <div class="chatTime">
+          ${time}
+        </div>
+
+      </div>
+
+      <div class="userBottom">
+
+        <div class="lastMsg">
+          ${user.lastMessage || user.text || ""}
+        </div>
+
+        ${
+          user.unread > 0
+            ? `<div class="unreadBadge">${user.unread}</div>`
+            : ""
+        }
+
+      </div>
+
+    </div>
+
+  `;
+
+  div.onclick = () => {
+
+    openChat(user.username);
+
+  };
+
+  sidebarUsers[user.username] = div;
+
+  usersBox.prepend(div);
+
+}
 
 function updateSidebarUser(user) {
 
